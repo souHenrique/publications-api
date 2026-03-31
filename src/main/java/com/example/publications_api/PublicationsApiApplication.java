3a80d3684bddd2eee7e3e16ec618c3cbb3ba6fb3
souHenrique <luishenriq.142@gmail.com>
2026-03-31 00:19:03 -0300

    Configuração inicial do projeto, apenas organizando para iniciar

---
+++ src/main/java/com/example/publications_api/PublicationsApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.publications_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublicationsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PublicationsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/publications_api/PublicationsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.publications_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PublicationsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
