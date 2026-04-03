03f4546d2739d138921dc03716dc4e22d0231d4b
souHenrique <luishenriq.142@gmail.com>
2026-04-03 11:39:08 -0300

    Iniciando as regras de negócio dos posts, criado método para criaçao do post

--- src/main/java/com/example/publications_api/service/PostService.java
+++ src/main/java/com/example/publications_api/service/PostService.java
@@ -1,4 +1,39 @@
 package com.example.publications_api.service;
 
+import com.example.publications_api.dto.post.PostRequestDTO;
+import com.example.publications_api.dto.post.PostResponseDTO;
+import com.example.publications_api.model.Post;
+import com.example.publications_api.model.User;
+import com.example.publications_api.repository.PostRepository;
+import com.example.publications_api.repository.UserRepository;
+
 public class PostService {
+
+    private final PostRepository postRepository;
+    private final UserRepository userRepository;
+
+    PostService (PostRepository postRepository, UserRepository userRepository) {
+        this.postRepository = postRepository;
+        this.userRepository = userRepository;
+    }
+
+    public PostResponseDTO createPost(PostRequestDTO postRequestDTO) {
+
+        Post post = new Post();
+
+        User existingUser = userRepository.findUserByIdUser(postRequestDTO.userId())
+                .orElseThrow(() -> new RuntimeException("Usuário não encontrado!"));
+
+        post.setUserId(existingUser);
+        post.setText(postRequestDTO.text());
+
+        return new PostResponseDTO(
+                existingUser.getUsername(),
+                post.getText(),
+                post.getCreatedAt(),
+                post.getUpdatedAt()
+        );
+    }
+
+
 }
