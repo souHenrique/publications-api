39933ac7bef324f3196e79a73f2f05bfbea82eb0
souHenrique <luishenriq.142@gmail.com>
2026-04-03 10:29:22 -0300

    Método para atualizar algum usuário já criado

--- src/main/java/com/example/publications_api/service/UserService.java
+++ src/main/java/com/example/publications_api/service/UserService.java
@@ -4,6 +4,7 @@ import com.example.publications_api.dto.user.UserRequestDTO;
 import com.example.publications_api.dto.user.UserResponseDTO;
 import com.example.publications_api.model.User;
 import com.example.publications_api.repository.UserRepository;
+import jakarta.transaction.Transactional;
 
 public class UserService {
 
@@ -34,5 +35,25 @@ public class UserService {
 
     public void findUserById(Long idUser) {
         userRepository.findUserByIdUser(idUser);
+    }
+
+    @Transactional
+    public UserResponseDTO updateUser(UserRequestDTO userRequestDTO, Long idUser) {
+
+        User existingUser = userRepository.findUserByIdUser(idUser)
+                .orElseThrow(() -> new RuntimeException("Usuário não encontrado!"));
+
+        existingUser.setUsername(userRequestDTO.username());
+        existingUser.setName(userRequestDTO.name());
+        existingUser.setEmail(userRequestDTO.email());
+        existingUser.setPassword(userRequestDTO.password());
+        existingUser.setBiography(userRequestDTO.biography());
+
+        return new UserResponseDTO(
+                existingUser.getUsername(),
+                existingUser.getName(),
+                existingUser.getEmail(),
+                existingUser.getBiography()
+        );
     }
 }
